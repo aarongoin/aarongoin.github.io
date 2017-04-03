3b852d854302fdee36bda4ba76b2e4cf7b7ac87f
Aaron Goin <aaron.goin@wsu.edu>
2017-04-02 17:45:58 -0700

    restructured file directory.

--- res/2017-2-27/rcc_example.js
+++ res/2017-2-27/rcc_example.js
@@ -44,41 +44,11 @@ con = new Console({
 	canvas: canvas,
 	fullscreen: false,
 	sprites: {
-		src: './img/rcc/sprites20.png',
+		src: 'res/2017-2-27/sprites20.png',
 		count: 16,
 		width: 20,
 		height: 20
 	},
-	colors: [
-        /* grayscale dark  */
-        '#000000', // 0 - black
-        '#333333', // 1
-        '#666666', // 2
-        /* grayscale light */
-        '#999999', // 3
-        '#cccccc', // 4
-        '#ffffff', // 5
-        /* red/orange      */
-        '#b21f35', // 6
-        '#d82735', // 7
-        '#ff7435', // 8
-        /* orange/yellow   */
-        '#ffa135', // 9
-        '#ffcb35', // 10
-        '#fff735', // 11
-        /* green           */
-        '#00753a', // 12
-        '#009e47', // 13
-        '#16dd36', // 14
-        /* blue            */
-        '#0052a5', // 15
-        '#0079e7', // 16
-        '#06a9fc', // 17
-        /* purple          */
-        '#681e7e', // 18
-        '#7d3cb5', // 19
-        '#bd7af6'  // 20
-    ],
 	onReady: function() {
 		setInterval(example, 1000/25);
         con.clear('#fff');
